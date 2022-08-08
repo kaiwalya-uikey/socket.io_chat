33c0ecb895ad66f894a95c0789e7cc6f4c149173
monty <kaiwalyauikey5@gmail.com>
2022-08-08 22:24:48 +0530

    editing document

--- js/client.js
+++ js/client.js
@@ -1,8 +1,12 @@
 const socket = io('http://localhost:8000');
 
-// DOM elements  
+// DOM elements  DOCUMENT OBJECT MODULE
+// The Document Object Model (DOM) is a programming interface for web documents.
+//  It represents the page so that programs can change the document structure, style, and content. 
+//  The DOM represents the document as nodes and objects; that way, programming languages can interact with the page.
 
-const form = document.getElementById('send-container');
+
+const form = document.getElementById('send-container');// taking all imports from index.html
 const messageInput = document.getElementById('messageInp')
 const messageContainer = document.querySelector(".container")
 
@@ -16,7 +20,7 @@ const append = (message, position)=>{
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
-    if(position =='left'){ 
+    if(position =='left'){ // left means another person is ending the message
         audio.play();
     }
 }
@@ -24,12 +28,23 @@ const append = (message, position)=>{
 
 
 const name = prompt("Enter your name to join"); // Ask new user for name 
+
+
 socket.emit('new-user-joined', name);
+
+// emit() to send a message to all the connected clients. 
+// This code will notify when a user connects to the server. 
+// io.on("connection", function(socket) { io.emit(“user connected”); });
+
+
 
 // If a new user joins, receive his/her name from the server
 socket.on('user-joined', name =>{
     append(`${name} joined the chat`, 'right')
 })
+// socket.on = The function to call when the event happens. Emitted when a message is received from the server. 
+// Emitted when a ping is received from the server. The client will automatically send back a pong.
+
 
 // If server sends a message, receive it
 socket.on('receive', data =>{
